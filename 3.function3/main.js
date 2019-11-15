59ac0b06fbfc73c5b29546d7656a6185c534e7a4
hithere <wongsiulunroy@gmail.com>
2019-11-15 15:27:47 +0800

    Create main.js

---
+++ 3.function3/main.js
@@ -0,0 +1,15 @@
+function alphabetSort(message){
+   var arr = message.split('');
+  var tmp;
+  for(var i = 0; i < arr.length; i++){
+    for(var j = i + 1; j < arr.length; j++){
+      if(arr[i] > arr[j]){
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr.join('');
+}
+alphabetSort('hello'); // should return 'ehllo'
